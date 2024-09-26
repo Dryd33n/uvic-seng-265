2c7638f43b130c6f59ace17ed9f497e136c2b4b5
Dryd3n <31601254+Dryd33n@users.noreply.github.com>
2024-09-26 02:25:32 -0700

    all tests pass
    idek what ive done at this point shoulda made more commits

--- a1/survey.c
+++ a1/survey.c
@@ -5,6 +5,7 @@
 
 #define MAX_RESPONDENTS 5
 #define NUM_QUESTIONS 38
+#define NUM_CATEGORIES 5
 #define NUM_LIKERTS 6
 
 #define MAX_LINE_LEN 4096
@@ -17,6 +18,13 @@ struct Question {
     char question[128]; //question string;
     _Bool directDirection; //true = direct, false = reverse, true = default
     char category[2];   //category: C,I,G,U,P
+};
+
+struct Respondee {
+    char program[32]; //respondee program
+    bool fromCanada;
+    char birthday[16];
+    char response[38][64];
 };
 
 
@@ -34,22 +42,23 @@ struct Question {
 void printIntro(const int numRespondents) {
     printf("Examining Science and Engineering Students' Attitudes Towards Computer Science\n");
     printf("SURVEY RESPONSE STATISTICS\n\n");
-    printf("NUMBER OF RESPONDENTS: %d\n\n", numRespondents);
+    printf("NUMBER OF RESPONDENTS: %d\n", numRespondents);
 }
 
 
 
 void printQuestionsWithRPF(struct Question * questions, float(* rpfArr)[NUM_LIKERTS], char(* likerts)[32]) {
-    printf("FOR EACH QUESTION BELOW, RELATIVE PERCENTUAL FREQUENCIES ARE COMPUTED FOR EACH LEVEL OF AGREEMENT\n");
+    printf("\nFOR EACH QUESTION BELOW, RELATIVE PERCENTUAL FREQUENCIES ARE COMPUTED FOR EACH LEVEL OF AGREEMENT");
 
     for (int i = 0; i < NUM_QUESTIONS; ++i) {
-        printf("\n%s", questions[i].question);
+        printf("\n\n%s", questions[i].question);
 
         for (int j = 0; j < NUM_LIKERTS; ++j) {
             printf("\n%.2f: ", rpfArr[i][j]);
             printf("%s", likerts[j]);
         }
     }
+    printf("\n");
 }
 
 
@@ -59,6 +68,7 @@ void printQuestionsWithRPF(struct Question * questions, float(* rpfArr)[NUM_LIKE
  */
 void readLine(char input[MAX_LINE_LEN]) {
     while(fgets(input, MAX_LINE_LEN, stdin)!= NULL) {
+        input[strcspn(input, "\n")] = '\0';
         if(input[0] != '#') {
             break; //stop reading from stdin when non comment line has been found
         }
@@ -172,7 +182,7 @@ void readLikerts(char(* likerts)[32]) {
 
     readLine(input);
 
-    const char *token = strtok(input, delim);
+    char *token = strtok(input, delim);
 
     while (token != NULL) {
         strcpy(likerts[i++], token);//copy each likert into array of likerts
@@ -183,22 +193,21 @@ void readLikerts(char(* likerts)[32]) {
 
 
 /**
- * Converts a given string containing a likert into its respective value as an int based on the direction of a question
- * @param likertStr the string containing the given likert to convert to int
+ * Converts a given string containing a likert into its respective value as an int
  * @param question the question for which the given likert is responding to, used to determine question direction
- * @param likerts the array of possible likerts
  * @return returns an integer 1,...,6 from given likertStr
  */
 int likertToVal(const char * likertStr,
-                const struct Question question,
                 const char(* likerts)[32])
 {
+    char cleanLikert[32];
+    strcpy(cleanLikert,likertStr);
+    cleanLikert[strcspn(cleanLikert, "\n")] = 0;
+
+
     for (int i = 0; i < 6; ++i) {//walk through all possible options for likerts
-        if(strcmp(likertStr, likerts[i]) == 0) {//compare likert[i] to give likert to see if they match
-            if(question.directDirection){
-                return i+1;
-            }
-            return 6-i;
+        if(strcmp(cleanLikert, likerts[i]) == 0) {//compare likert[i] to give likert to see if they match
+            return i+1;
         }
     }
     return -1;  // default case if likert is not found
@@ -206,61 +215,50 @@ int likertToVal(const char * likertStr,
 
 
 
-/**
- * Parses each response line and stores the response values in a unique row of a int matrix.
- * @param responseArr give row of response matrix to be filled
- * @param input string containing response + respondee information
- * @param questions array of questions
- * @param likerts array of all likerts
- */
-void tokenizeResponse(int * responseArr,
-                      char * input,
-                      const struct Question * questions,
-                      const char(* likerts)[32])
-{
+void constructRespondent(char input[4096], struct Respondee *respondee) {
     const char delim[2] = ",";
     int i = 0;
 
     const char *token = strtok(input, delim);
 
-    while (token != NULL) {
-        if(i > 2) {//skip respondee info
-            responseArr[i-3]= likertToVal(token, questions[i-3], likerts);//set value to array from likert
-        }
+     while (token != NULL) {
+         if(i > 2) {
+             strncpy(respondee->response[i-3], token, 32);
+         }else if(i==2) {
+             strcpy(respondee->birthday, token);
+         }else if(i==1) {
+             if(strcmp("yes", token) == 0) {
+                respondee->fromCanada = true;
+             }else {
+                 respondee->fromCanada = false;
+             }
+         }else if(i==0){
+             strcpy(respondee->program, token);
+         }
 
-        token = strtok(NULL, delim);
-        i++;
-    }
+         token = strtok(NULL, delim);
+         i++;
+     }
 }
 
 
-/**
- * Reads all responses them and process them for further tokenization
- * @param responseArr the response int matrix to be filled
- * @param questions the array of all questions
- * @param likerts the array of all likerts
- * @return returns and integer containing the amount of respondents to the survey
- */
-int readResponses(int(* responseArr)[MAX_RESPONDENTS],
-                  const struct Question * questions,
-                  const char(* likerts)[32])
-{
-    char input[MAX_LINE_LEN];
+
+int readResponses(struct Respondee respondees[MAX_RESPONDENTS]) {
     int i = 0;
 
-    while (fgets(input, MAX_LINE_LEN, stdin)!=NULL) {//continuously read from stdin
-        if(input[0] != '#') {//ignore line if it is a comment line and keep reading
-            tokenizeResponse(responseArr[i], input, questions, likerts);//send off response line for tokenization
+     while ( i < MAX_RESPONDENTS) {
+         char input[MAX_LINE_LEN*5];
+         fgets(input, sizeof(input), stdin);
 
-            for (int j = 0; j < 38; ++j) {
-                printf("%d",responseArr[i][j]);
-            }
-            i++;
-        }
-    }
+         if(input[0] != '#') {//ignore line if it is a comment line and keep reading
+             constructRespondent(input, &respondees[i]);
+             i++;
+         }
+     }
 
     return i;
 }
+
 
 
 
@@ -294,48 +292,108 @@ void initializeEmptyResponsesRPF(float(* responseRPFArr)[NUM_LIKERTS]) {
 
 
 
-void summateResponses(int responseArr[NUM_QUESTIONS][MAX_RESPONDENTS],
-                      int(* responseSums)[NUM_LIKERTS],
-                      const struct Question * questions)
-{
-    for (int i = 0; i < NUM_QUESTIONS; ++i) {           //for each question
-        bool direct = questions[i].directDirection;
+void summateResponses(const struct Respondee respondees[5], const int numRespondents, int(* responsesSum)[6], const char(* likerts)[32]) {
+    for (int i = 0; i < numRespondents; ++i) {
+        for (int j = 0; j < NUM_QUESTIONS; ++j) {
+            const int likertVal = likertToVal(respondees[i].response[j], likerts);
+            responsesSum[j][likertVal-1]++;
+        }
+    }
 
-        for (int j = 0; j < MAX_RESPONDENTS; ++j) {     //for each respondent response
-            if(direct) {
-                responseSums[i][responseArr[i][j]]++;   //
+
+
+}
+
+void computeRPFArr(float(* responsesRPF)[6], int(* responsesSum)[6], int numRespondants) {
+    for (int i = 0; i < NUM_QUESTIONS; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            responsesRPF[i][j] = ((float)responsesSum[i][j] / (float)numRespondants) * 100;
+        }
+    }
+}
+
+void computeResponsesScoreArr(struct Respondee * respondees, int numRespondents, int(* responseScores)[NUM_QUESTIONS], struct Question * questions,  const char(* likerts)[32]) {
+    for (int i = 0; i < numRespondents; ++i) {
+        for (int j = 0; j < NUM_QUESTIONS; ++j) {
+            int likertVal = likertToVal(respondees[i].response[j], likerts);
+            int likertValDirected = likertVal;
+            if(!questions[j].directDirection) {
+                likertValDirected = 7 - likertVal;
+            }
+            responseScores[i][j] = likertValDirected;
+        }
+    }
+}
+
+void computeResponsesCatScoreArr(float(* responsesCatScores)[5], const int(* responsesScores)[38], const struct Question * questions, const int numRespondents) {
+
+    for (int i = 0; i < numRespondents; ++i) {
+        int cCount=0, iCount=0, gCount=0, uCount=0, pCount=0;
+        int cSum=0, iSum=0, gSum=0, uSum=0, pSum = 0;
+
+        for (int j = 0; j < NUM_QUESTIONS; ++j) {
+            const char *category = questions[j].category;
+
+            if(strcmp("C",category) == 0) {
+                cCount++;
+                cSum +=responsesScores[i][j];
+            }else if(strcmp("I",category) == 0) {
+                iCount++;
+                iSum +=responsesScores[i][j];
+            }else if(strcmp("G",category) == 0) {
+                gCount++;
+                gSum +=responsesScores[i][j];
+            }else if(strcmp("U",category) == 0) {
+                uCount++;
+                uSum +=responsesScores[i][j];
+            }else if(strcmp("P",category) == 0) {
+                pCount++;
+                pSum +=responsesScores[i][j];
             }else {
-                responseSums[i][7-responseArr[i][j]]++;
+                printf("error");
             }
         }
+        responsesCatScores[i][0] = (float)cSum / cCount;
+        responsesCatScores[i][1] = (float)iSum / iCount;
+        responsesCatScores[i][2] = (float)gSum / gCount;
+        responsesCatScores[i][3] = (float)uSum / uCount;
+        responsesCatScores[i][4] = (float)pSum / pCount;
+    }
+
+}
+
+void printResponseCatScores(float(* catScores)[5], int numRespondents) {
+        printf("\nSCORES FOR ALL THE RESPONDENTS\n\n");
+
+    for (int i = 0; i < numRespondents; ++i) {
+        printf("C:%.2f,I:%.2f,G:%.2f,U:%.2f,P:%.2f\n",catScores[i][0],catScores[i][1],catScores[i][2],catScores[i][3],catScores[i][4]);
     }
 }
 
-void calculateRPF(const int num_respondents,
-                  const int(* responseSums)[NUM_LIKERTS],
-                  float(* responses_rpf)[NUM_LIKERTS])
-{
-    if(num_respondents == 0) exit(133);
-
-    for (int i = 0; i < NUM_QUESTIONS; ++i) {
-        for (int j = 0; j < NUM_LIKERTS; ++j) {
-            responses_rpf[i][j] = ((float)responseSums[i][j] / (float)num_respondents)*100;
+void computerAvgCatScores(float * avgCatScore, float(* catScores)[NUM_CATEGORIES], int numRespondents) {
+    for (int i = 0; i < NUM_CATEGORIES; ++i) {
+        float catSum = 0;
+        for (int j = 0; j < numRespondents; ++j) {
+            catSum += catScores[j][i];
         }
+        avgCatScore[i] = catSum / (float)numRespondents;
     }
 }
 
+void printAvgCatScores(float * avgCatScores) {
+    printf("\nAVERAGE SCORES PER RESPONDENT\n\n");
+    printf("C:%.2f,I:%.2f,G:%.2f,U:%.2f,P:%.2f\n",avgCatScores[0],avgCatScores[1],avgCatScores[2],avgCatScores[3],avgCatScores[4]);
+}
 
 int main() {
     int config[4];                  //Configuration
     struct Question questions[NUM_QUESTIONS];  //Array Of Questions
     char likerts[NUM_LIKERTS][32];
     int responses[NUM_QUESTIONS][MAX_RESPONDENTS];
-    int responsesSum[NUM_QUESTIONS][NUM_LIKERTS];
     float responsesRPF[NUM_QUESTIONS][NUM_LIKERTS];
-    int numRespondents;
+    int numRespondents = 0;
 
     initializeEmptyResponses(responses);
-
     readConfig(config);
     readQuestions(questions);
     readDirections(questions);
@@ -347,13 +405,35 @@ int main() {
         printIntro(numRespondents);
         printQuestionsWithRPF(questions, responsesRPF, likerts);
     }else{
-        
+        struct Respondee respondents[MAX_RESPONDENTS];
+        numRespondents = readResponses(respondents);
+        printIntro(numRespondents);
+
         if(config[1]==1) {
-            numRespondents = readResponses(responses, questions, likerts);
-            summateResponses(responses,responsesSum, questions);
-            calculateRPF(numRespondents, responsesSum, responsesRPF);
-            printIntro(numRespondents);
+            int responsesSum[NUM_QUESTIONS][NUM_LIKERTS];
+            summateResponses(respondents, numRespondents, responsesSum, likerts);
+            computeRPFArr(responsesRPF, responsesSum, numRespondents);
             printQuestionsWithRPF(questions, responsesRPF, likerts);
+        }
+
+
+        if(config[2]==1 || config [3] == 1) {
+            int responsesScores[numRespondents][NUM_QUESTIONS];
+            float responsesCatScores[numRespondents][NUM_CATEGORIES];
+            computeResponsesScoreArr(respondents, numRespondents, responsesScores, questions, likerts);
+            computeResponsesCatScoreArr(responsesCatScores, responsesScores, questions, numRespondents);
+
+            if(config[2] == 1) {
+                printResponseCatScores(responsesCatScores, numRespondents);
+            }
+
+            if(config[3] == 1) {
+                float avgCatScores[NUM_CATEGORIES];
+                computerAvgCatScores(avgCatScores, responsesCatScores, numRespondents);
+                printAvgCatScores(avgCatScores);
+            }
+
+
         }
     }
 
